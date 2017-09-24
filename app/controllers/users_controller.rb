403531ef330482fee62164672a506ca35b4f4e37
Charles Anderson <charlesandreanderson22@gmail.com>
2017-09-24 09:12:06 -0700

    added users controller and pesudo code

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+    # show specific user
+  end
+
+  def new
+    # start to create new user
+  end
+
+  def create
+    # create new user
+  end
+
+  def edit
+    #edit existing user
+  end
+
+  def update
+    # update existing user
+  end
+
+  def destroy
+    # delete user completely
+  end
+end
