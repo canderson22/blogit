7a8107b50e7dce0b6f64edbebac23397d079fca9
Charles Anderson <charlesandreanderson22@gmail.com>
2017-09-25 14:07:45 -0700

    finish editing profile functionality and instated a category model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,15 +30,49 @@ class UsersController < ApplicationController
   end
 
   def edit
-    #edit existing user
+    @user = User.find(params[:id])
+    if current_user.id != @user.id
+      flash[:warning] = "You dont have access!"
+      redirect_to user_path(session[:user_id])
+    end
   end
 
   def update
     # update existing user
+    begin
+    @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      # handle not found error
+      flash[:danger] = "That page does not exist."
+      redirect_to user_path(current_user)
+    rescue ActiveRecord::ActiveRecordError
+      # handle other ActiveRecord errors
+      flash[:warning] = "Sorry there was an error."
+    rescue # StandardError
+      # handle most other errors
+    rescue Exception
+      # handle everything else
+      @user.update_attributes(user_params)
+      flash[:success] = "Profile successfully updated"
+      redirect_to user_path(@user)
+    else
+      flash[:warning] = "Please check all fields"
+      redirect_to edit_user_path(@user)
+    end
+ 
   end
 
   def destroy
     # delete user completely
+    @user = User.find(params[:id])
+    if current_user.id != @user.id
+      flash[:warning] = "You dont have access!"
+      redirect_to user_path(session[:user_id])
+    end
+    @user.destroy
+    session[:user_id] = nil
+    flash[:danger] = "User page deleted!"
+    redirect_to users_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users
-
+  post '/users/:id/edit' => 'users#update'
+  post '/users/:id/delete' => 'users#destroy', as: :delete_user
   resources :sessions, only: [:new, :create]
   delete '/logout' => 'sessions#destroy', as: :logout
 end
