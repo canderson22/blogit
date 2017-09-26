c838a1578ac9a6f3e6daf3f2f304ee7cb7cc25cb
Charles Anderson <charlesandreanderson22@gmail.com>
2017-09-26 13:08:41 -0700

    created comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170925232539) do
+ActiveRecord::Schema.define(version: 20170926200424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,16 @@ ActiveRecord::Schema.define(version: 20170925232539) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.bigint "blog_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "f_name"
     t.string "l_name"
@@ -43,4 +53,6 @@ ActiveRecord::Schema.define(version: 20170925232539) do
 
   add_foreign_key "blogs", "categories"
   add_foreign_key "blogs", "users"
+  add_foreign_key "comments", "blogs"
+  add_foreign_key "comments", "users"
 end
